f1a4923a06620c63e2b7bc46a35fb4167f3087fe
codeyumm <pvashi006@gmail.com>
2024-04-11 21:51:11 -0400

    First Commit

---
+++ components/nav/navBar.jsx
@@ -0,0 +1,15 @@
+import './navBar.css'
+
+export default function NavBar(){
+    return(
+
+        <header>
+            <nav>
+
+                <h1>CricStatsBattle</h1>
+
+            </nav>
+        </header>
+
+    )
+}
